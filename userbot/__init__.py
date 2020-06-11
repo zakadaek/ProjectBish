5ab723fddb55edfcd8413c88fb6a65e815ea2f06
BianSepang <bianconneri46@gmail.com>
2020-06-11 18:55:03 +0800

    ProjectBish: removed some bits
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -165,20 +165,6 @@ GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN", None)
 # Quote
 QUOTES_API_TOKEN = os.environ.get("QUOTES_API_TOKEN", None)
 
-# Inline bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
-
-# Genius Lyrics API
-GENIUS = os.environ.get("GENIUS_ACCESS_TOKEN", None)
-CMD_HELP = {}
-
-# IMG Stuff
-IMG_LIMIT = os.environ.get("IMG_LIMIT", None)
-
-# JustWatch Country
-WATCH_COUNTRY = os.environ.get("WATCH_COUNTRY", None)
-
 # Deezloader
 DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
 
