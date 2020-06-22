3e30baec7e57f0c1661e50eacb4c6be0cdc4fd85
BianSepang <bianconneri46@gmail.com>
2020-06-23 00:12:44 +0800

    system_stats: update
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -133,9 +133,9 @@ async def amireallyalive(alive):
     logo = ALIVE_LOGO
     output = (f"`ProjectBish` is running on `{UPSTREAM_REPO_BRANCH}`\n"
              f"====================================\n"
-             f"👤 User     : {DEFAULTUSER}\n"
-             f"🐍 Python   : v{python_version()}\n"
-             f"⚙️ Telethon : v{version.__version__}\n"
+             f"🐍 `Python    :` v{python_version()}\n"
+             f"⚙️ `Telethon  :` v{version.__version__}\n"
+             f"👤 `User      :` {DEFAULTUSER}\n"
              f"====================================\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
