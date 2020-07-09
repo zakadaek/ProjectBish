7b79ab4ce70bce85d42033ed2b551308227df0d8
Angga <angga@linuxmail.org>
2020-07-09 15:26:43 +0700

    paste: improve dogbin and add nekobin
    Change-Id: Id5532eea9dea9f7da015a3d8229d9a3af86bb58f
    Signed-off-by: Angga <angga@linuxmail.org>
    Signed-off-by: BianSepang <bianconneri46@gmail.com>

--- userbot/modules/paste.py
+++ userbot/modules/paste.py
@@ -53,14 +53,14 @@ async def paste(pstl):
 
         if response['isUrl']:
             reply_text = ("`Pasted successfully!`\n\n"
-                          f"`Shortened URL:` {dogbin_final_url}\n\n"
+                          f"[Shortened URL]({dogbin_final_url})\n\n"
                           "`Original(non-shortened) URLs`\n"
-                          f"`Dogbin URL`: {DOGBIN_URL}v/{key}\n"
-                          f"`View RAW`: {DOGBIN_URL}raw/{key}")
+                          f"[Dogbin URL]({DOGBIN_URL}v/{key})\n"
+                          f"[View RAW]({DOGBIN_URL}raw/{key})")
         else:
             reply_text = ("`Pasted successfully!`\n\n"
-                          f"`Dogbin URL`: {dogbin_final_url}\n"
-                          f"`View RAW`: {DOGBIN_URL}raw/{key}")
+                          f"[Dogbin URL]({dogbin_final_url})\n"
+                          f"[View RAW]({DOGBIN_URL}raw/{key})")
     else:
         reply_text = ("`Failed to reach Dogbin`")
 
@@ -122,10 +122,58 @@ async def get_dogbin_content(dog_url):
         )
 
 
+@register(outgoing=True, pattern=r"^\.neko(?: |$)([\s\S]*)")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    input_str = event.pattern_match.group(1)
+    message = "SYNTAX: `.neko <long text to include>`"
+    if input_str:
+        message = input_str
+    elif event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        if previous_message.media:
+            downloaded_file_name = await event.client.download_media(
+                previous_message,
+                TEMP_DOWNLOAD_DIRECTORY,
+                progress_callback=progress
+            )
+            m_list = None
+            with open(downloaded_file_name, "rb") as fd:
+                m_list = fd.readlines()
+            message = ""
+            for m in m_list:
+                # message += m.decode("UTF-8") + "\r\n"
+                message += m.decode("UTF-8")
+            os.remove(downloaded_file_name)
+        else:
+            message = previous_message.message
+    else:
+        message = "SYNTAX: `.neko <long text to include>`"
+    py_file =  ""
+    if downloaded_file_name.endswith(".py"):
+        py_file += ".py"
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}{py_file}'
+        reply_text = f'Pasted to [Nekobin]({url})'
+        await event.edit(reply_text)
+    else:
+        data = message
+        key = requests.post('https://nekobin.com/api/documents', json={"content": data}).json().get('result').get('key')
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'Pasted to [Nekobin]({url})'
+        await event.edit(reply_text)
+
 CMD_HELP.update({
-    "dogbin":
+    "paste":
     ">`.paste <text/reply>`"
     "\nUsage: Create a paste or a shortened url using dogbin (https://del.dog/)"
+    "\n\n>`.neko <text/reply>"
+    "\nUsage: Same as `.paste` but use Nekobin"
     "\n\n>`.getpaste`"
     "\nUsage: Gets the content of a paste or shortened url from dogbin (https://del.dog/)"
 })
